4e61b89ac01da412856189646d4337496b5ac705
Sy <sy-unimportant@outlook.com>
2021-11-16 13:23:24 +0000

    created branch calc-array, to try and use an array to display numbers on the screen from user input

--- javascript.js
+++ javascript.js
@@ -1,4 +1,14 @@
+let num1 = 0;
 
+
+const numbers = document.querySelectorAll('button');
+numbers.forEach((number) => {
+    number.addEventListener('click', () => {
+            operate(number.id);
+    })
+});
+
+/*
 const numbers = document.querySelectorAll('button');
 numbers.forEach((number) => {
     number.addEventListener('click', () => {
@@ -6,11 +16,27 @@ numbers.forEach((number) => {
         console.log(typeof(number.id));
     })
 });
+*/
 
 
+const SCREEN = document.querySelector('.screen')
+const SCREENTEXT = document.createElement('div')
+SCREENTEXT.classList.add('screenText');
+SCREENTEXT.textContent = num1;
+SCREEN.appendChild(SCREENTEXT);
+
+// The main function of the program
+function operate(n){
+
+    let sum = n;
+
+    
+    console.log(n + ", Gotcha!");
+}
 
 function add (a, b)
 {
+    
     return a + b;
 }
 
