fb580019e5d27a3b2282a239931127b9726d9fef
Sy <sy-unimportant@outlook.com>
2021-11-16 15:32:49 +0000

    added add feature. Trying to figure out how to concatanate after

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 let num1 = 0;
-const input = [];
+let num2 = 0;
+let operand = "none";
 
 const numbers = document.querySelectorAll('button');
 numbers.forEach((number) => {
@@ -17,23 +18,58 @@ SCREEN.appendChild(SCREENTEXT);
 // The main function of the program
 function operate(n){
 
-    if (num1 === 0) {
-        num1 = n;
-        SCREENTEXT.textContent = n;
+    if (operand != "none"){  
+
+
+        if (num1 === 0) { // Add label if anotehr operator clicked to avoid crash
+            num1 = n;
+            SCREENTEXT.textContent = n;
+        }
+
+
+        else {
+            num1 += n;
+            SCREENTEXT.textContent = num1;
+        }
+
+        switch (operand){
+            case "add":
+                num1 = 0;
+                num1 += n;
+                add(num2 + num1);
+                break;
+        }
+
+        //parse before adding
+        //console.log(num2 + num1);
+
     }
 
     else {
-        num1 += n;
+        if (num1 === 0) {
+            num1 = n;
+            SCREENTEXT.textContent = n;
+        }
 
-        SCREENTEXT.textContent = num1;
+        else if (n === "add"){
+            num2 = num1;
+            num1 = 0;
+            operand = "add";
+        }
+
+        else {
+            num1 += n;
+            SCREENTEXT.textContent = num1;
+        }
     }
 
 }
 
 function add (a, b)
 {
-    
+    console.log(a + b);
     return a + b;
+    
 }
 
 function subtract (a, b) 
